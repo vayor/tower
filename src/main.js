86bf6c5598ffda5c7b79194cf7839f809e4cd5ed
Shyamin Ayesh <me@shyamin.com>
2023-09-23 19:16:24 +0530

    fix wrappers

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue"
+import App from "./App.vue"
 
-import './assets/css/style.scss'
+import "./assets/css/style.scss"
 
 createApp(App).mount('#app')
