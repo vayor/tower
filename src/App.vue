2cd98ea8b4c416a75dda7976236fc4ed5907bb8b
Shyamin Ayesh <me@shyamin.com>
2023-09-24 23:09:00 +0530

    devide screen

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,9 @@
 <template>
+    <div class="flex flex-col min-h-screen">
+        <div class="flex-none bg-teal-500">1</div>
+        <div class="grow bg-teal-800">2</div>
+        <div class="flex-none bg-teal-500">3</div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
